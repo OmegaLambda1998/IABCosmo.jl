d5eb3bfa748a906a33059c32f9b4a9c9f198f198
Documenter.jl <documenter@juliadocs.github.io>
2023-05-08 01:34:13 +0000

    build based on 65d2acb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#IABCosmo-Documentation","page":"IABCosmo","title":"IABCosmo Documentation","text":"","category":"section"},{"location":"","page":"IABCosmo","title":"IABCosmo","text":"","category":"page"},{"location":"#Install","page":"IABCosmo","title":"Install","text":"","category":"section"},{"location":"","page":"IABCosmo","title":"IABCosmo","text":"``` using Pkg Pkg.add(\"IABCosmo\")","category":"page"}]
+}
