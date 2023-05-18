0c27f8fddb8aa1e789d74eb16a683af58ea95fda
Patrick Armstrong <patrick.armstrong@anu.edu.au>
2023-05-18 12:22:24 +1000

    Fix path

--- src/RunModule.jl
+++ src/RunModule.jl
@@ -14,7 +14,7 @@ export run_IABCosmo
 function run_IABCosmo(toml::Dict)
     covariance_matrix = run_JLACovarianceMatrix(toml)
     jacobian, _ = run_SALTJacobian(toml)
-    simulator = Simulator(toml["SIM"], covariance_matrix, jacobian)
+    simulator = Simulator(toml["SIM"], covariance_matrix, jacobian, toml["GLOBAL"])
     simulate = get(toml["SIM"], "SIMULATE", 0)
     for sim in 1:simulate
         simulate(simulator, sim)
--- src/SimulatorModule.jl
+++ src/SimulatorModule.jl
@@ -19,8 +19,11 @@ struct Simulator
     jacobian::Jacobian
 end
 
-function Simulator(config::Dict{String,Any}, covariance_matrix::CovarianceMatrix, jacobian::Jacobian)
+function Simulator(config::Dict{String,Any}, covariance_matrix::CovarianceMatrix, jacobian::Jacobian, global_config::Dict{String,Any})
     pippin_template_input = config["PIPPIN_TEMPLATE"]
+    if !isabspath(pippin_template_input)
+        pippin_template_input = joinpath(global_config["BASE_PATH"], pippin_template_input)
+    end
     pippin_template = YAML.load_file(abspath(pippin_template_input); dicttype=OrderedDict{String,Any})
     @show pippin_template
     return Simulator(pippin_template, covariance_matrix, jacobian)
