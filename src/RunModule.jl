8140400f66c45d525783d1b821b929390e70006d
Patrick Armstrong <patrick.armstrong@anu.edu.au>
2023-05-18 12:18:00 +1000

    Import pippin_template

--- src/RunModule.jl
+++ src/RunModule.jl
@@ -14,11 +14,10 @@ export run_IABCosmo
 function run_IABCosmo(toml::Dict)
     covariance_matrix = run_JLACovarianceMatrix(toml)
     jacobian, _ = run_SALTJacobian(toml)
-    template = toml["PIPPIN_TEMPLATE"]
     simulator = Simulator(toml["SIM"], covariance_matrix, jacobian)
     simulate = get(toml["SIM"], "SIMULATE", 0)
     for sim in 1:simulate
-        simulator.simulate(sim)
+        simulate(simulator, sim)
     end
 end
 
--- src/SimulatorModule.jl
+++ src/SimulatorModule.jl
@@ -2,18 +2,31 @@
 module SimulatorModule
 
 # Internal Packages 
+using OrderedCollections
+using YAML
 
 # External Packages 
 using JLACovarianceMatrix
 using SALTJacobian
 
 # Exports
+export Simulator
+export simulate
 
-struct Simulation
+struct Simulator
+    pippin_template::OrderedDict{String,Any}
     covariance_matrix::CovarianceMatrix
-    jacobian::Jacobian    
+    jacobian::Jacobian
 end
 
+function Simulator(config::Dict{String,Any}, covariance_matrix::CovarianceMatrix, jacobian::Jacobian)
+    pippin_template_input = config["PIPPIN_TEMPLATE"]
+    pippin_template = YAML.loadfile(abspath(pippin_template_input); dicttype=OrderedDict{String,Any})
+    @show pippin_template
+    return Simulator(pippin_template, covariance_matrix, jacobian)
+end
 
+function simulate(simulator::Simulator, sim_num::Int64)
+end
 
 end
