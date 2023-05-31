908c9f3496be7bb29dde3c1e99c17c03c8a47e29
Patrick Armstrong <patrick.armstrong@anu.edu.au>
2023-05-31 00:19:45 -0500

    Load precreated jacobian

--- src/RunModule.jl
+++ src/RunModule.jl
@@ -15,7 +15,9 @@ function get_covariance_matrix(toml::Dict{String, Any})
     config = get(toml, "COVARIANCEMATRIX", Dict{String, Any}())
     name = get(config, "NAME", "DES")
     input = joinpath(toml["GLOBAL"]["OUTPUT_PATH"], "$(name).jld2")
+    @debug "Testing if Covariance Matrix $input exists"
     if isfile(input)
+        @info "Loading Covariance Matrix from $input"
         covariance_matrix = loadCovarianceMatrix(input)
     else
         covariance_matrix = run_JLACovarianceMatrix(toml)
@@ -30,8 +32,10 @@ function get_jacobian(toml::Dict{String, Any})
         name *= "_jacobian"
     end
     input = joinpath(toml["GLOBAL"]["OUTPUT_PATH"], "$(name).jld2") 
+    @debug "Testing if Jacobian $input exists"
     if isfile(input)
-        toml["JACOBIAN_PATH"] = input
+        @info "Loading Jacobian from $input"
+        toml["JACOBIAN"]["JACOBIAN_PATH"] = input
     end
     jacobian, _ = run_SALTJacobian(toml)
     return jacobian
@@ -39,6 +43,7 @@ end
 
 function run_IABCosmo(toml::Dict{String, Any})
     covariance_matrix = get_covariance_matrix(toml)
+    jacobian = get_jacobian(toml)
     simulator = Simulator(toml["SIM"], covariance_matrix, jacobian, toml["GLOBAL"])
     num_sims = get(toml["SIM"], "SIMULATE", 0)
     for sim in 1:num_sims
