086cdaef679ac40611076c52b06c23e652f5ebaf
Patrick Armstrong <patrick.armstrong@anu.edu.au>
2023-05-31 11:24:22 +1000

    Load precomputed jacobian

--- src/RunModule.jl
+++ src/RunModule.jl
@@ -11,8 +11,20 @@ using .SimulatorModule
 # Exports
 export run_IABCosmo
 
-function run_IABCosmo(toml::Dict)
-    covariance_matrix = run_JLACovarianceMatrix(toml)
+function get_covariance_matrix(toml::Dict{String, Any})
+    config = get(toml, "COVARIANCEMATRIX", Dict{String, Any}())
+    name = get(config, "NAME", "DES")
+    input = joinpath(toml["GLOBAL"]["OUTPUT_PATH"], "$(name).jld2")
+    if isfile(input)
+        covariance_matrix = loadCovarianceMatrix(input)
+    else
+        covariance_matrix = run_JLACovarianceMatrix(toml)
+    end
+    return covariance_matrix
+end
+
+function run_IABCosmo(toml::Dict{String, Any})
+    covariance_matrix = get_covariance_matrix(toml)
     jacobian, _ = run_SALTJacobian(toml)
     simulator = Simulator(toml["SIM"], covariance_matrix, jacobian, toml["GLOBAL"])
     num_sims = get(toml["SIM"], "SIMULATE", 0)
